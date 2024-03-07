9cb7f1c9ced03f574b8d198ee12f001feec07400
bernardjbs <bernardjbs@yahoo.com>
2024-03-07 19:16:16 +0800

    Match Model and Migration

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import sequelize from '@config/connection.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 sequelize.sync().then(() => {
+  console.log('hello')
   app.listen(PORT, () => {
     console.log(`Server HAO is running on http://localhost:${PORT}`);
   });
