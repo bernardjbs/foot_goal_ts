2b046b0f7e345e90bccb4a0913fed011c6ada322
bernardjbs <bernardjbs@yahoo.com>
2024-03-07 20:19:22 +0800

    Upgrading sequelize from 6 to 7 for ESM compatibility

--- src/database/config.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-const directoryPath = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(directoryPath, '../../.env') });
-export const { PORT, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_DATABASE } = { ...process.env } as {
-  [key: string]: string;
-};
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,20 @@
 import { Sequelize } from 'sequelize';
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USERNAME } from './config.js';
+import { processEnv } from '@utils/processEnv.js';
+
+const DB_DATABASE = processEnv().DB_DATABASE as string;
+const DB_USERNAME = processEnv().DB_USERNAME as string;
+const DB_PASSWORD = processEnv().DB_PASSWORD as string;
 
 let sequelizeConnection: Sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
+  host: processEnv().DB_HOST,
   dialect: 'mysql',
   port: 3306, 
 });
 
+try {
+  await sequelizeConnection.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 export default sequelizeConnection;
