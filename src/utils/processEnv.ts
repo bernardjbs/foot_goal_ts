57ed4cc6e9a79ce54fec1a5c45fdd72846ca2694
bernardjbs <bernardjbs@yahoo.com>
2024-03-25 23:37:52 +0800

    Removed debugging console log

--- src/utils/processEnv.ts
+++ src/utils/processEnv.ts
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(__dirname);
 dotenv.config({
   path: path.resolve(__dirname, '../../.env')
 });
